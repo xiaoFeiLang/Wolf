27713d336954052bd677b72c382ac6c690953426
627265625@qq.com <xiao99L>
2017-11-19 17:37:55 +0800

    test DB

--- TestWinForm/Form1.Designer.cs
+++ TestWinForm/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CreateTestDB = new System.Windows.Forms.Button();
+            this.InsertData = new System.Windows.Forms.Button();
+            this.SelectData = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CreateTestDB
+            // 
+            this.CreateTestDB.Location = new System.Drawing.Point(43, 18);
+            this.CreateTestDB.Name = "CreateTestDB";
+            this.CreateTestDB.Size = new System.Drawing.Size(75, 23);
+            this.CreateTestDB.TabIndex = 0;
+            this.CreateTestDB.Text = "Create";
+            this.CreateTestDB.UseVisualStyleBackColor = true;
+            this.CreateTestDB.Click += new System.EventHandler(this.CreateTestDB_Click);
+            // 
+            // InsertData
+            // 
+            this.InsertData.Location = new System.Drawing.Point(185, 18);
+            this.InsertData.Name = "InsertData";
+            this.InsertData.Size = new System.Drawing.Size(75, 23);
+            this.InsertData.TabIndex = 1;
+            this.InsertData.Text = "Insert";
+            this.InsertData.UseVisualStyleBackColor = true;
+            this.InsertData.Click += new System.EventHandler(this.InsertData_Click);
+            // 
+            // SelectData
+            // 
+            this.SelectData.Location = new System.Drawing.Point(315, 18);
+            this.SelectData.Name = "SelectData";
+            this.SelectData.Size = new System.Drawing.Size(75, 23);
+            this.SelectData.TabIndex = 2;
+            this.SelectData.Text = "Select";
+            this.SelectData.UseVisualStyleBackColor = true;
+            this.SelectData.Click += new System.EventHandler(this.SelectData_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(370, 271);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 362);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.SelectData);
+            this.Controls.Add(this.InsertData);
+            this.Controls.Add(this.CreateTestDB);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CreateTestDB;
+        private System.Windows.Forms.Button InsertData;
+        private System.Windows.Forms.Button SelectData;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- TestWinForm/Form1.cs
+++ TestWinForm/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ADOX;
+using System.Data.OleDb;
 
 namespace TestWinForm
 {
@@ -15,6 +17,84 @@ namespace TestWinForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void CreateTestDB_Click(object sender, EventArgs e)
+        {
+            string dir = AppDomain.CurrentDomain.BaseDirectory;
+            string dbName = dir + "test.mdb";
+            ADOX.CatalogClass catlog = new ADOX.CatalogClass();
+            catlog.Create("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + dbName + ";");
+            ADOX.TableClass tableClass = new ADOX.TableClass();
+            tableClass.ParentCatalog = catlog;
+            tableClass.Name = "TestTable";
+
+            ADOX.ColumnClass columnID = new ADOX.ColumnClass();
+            columnID.ParentCatalog = catlog;
+            columnID.Type = ADOX.DataTypeEnum.adInteger;
+            columnID.Name = "Id";
+            columnID.Properties["Jet OLEDB:Allow Zero Length"].Value = false;
+            columnID.Properties["AutoIncrement"].Value = true;
+            tableClass.Columns.Append(columnID, ADOX.DataTypeEnum.adInteger, 0);
+
+            ADOX.ColumnClass columnName = addCol(catlog, "Name");
+            tableClass.Columns.Append(columnName, ADOX.DataTypeEnum.adVarChar, 30);
+
+            catlog.Tables.Append(tableClass);
+            MessageBox.Show("Successful");
+            tableClass = null;
+            catlog = null;
+        }
+
+        private ADOX.ColumnClass addCol(ADOX.CatalogClass cat, string colName)
+        {
+            ADOX.ColumnClass col = new ADOX.ColumnClass();
+            col.ParentCatalog = cat;
+            // col.Type = ADOX.DataTypeEnum.adInteger; // 必须先设置字段类型
+            col.Name = colName;
+            col.Properties["Jet OLEDB:Allow Zero Length"].Value = false;
+            return col;
+        }
+
+        private void InsertData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OleDbConnection conn = getConn();
+                conn.Open();
+                for(int i = 0; i< 10; i++)
+                {
+                    string sql = "insert into TestTable (name) values ('";
+                    sql += "A" + i.ToString();
+                    sql += "')";
+                    OleDbCommand cmd = new OleDbCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private OleDbConnection getConn()
+        {
+            string dir = AppDomain.CurrentDomain.BaseDirectory + "test.mdb";
+            string connstring = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + dir + ";";
+            OleDbConnection conn = new OleDbConnection(connstring);
+            return conn;
+        }
+
+        private void SelectData_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = getConn();
+            conn.Open();
+            string sql = "select * from TestTable";
+            OleDbDataAdapter da = new OleDbDataAdapter(sql, conn);
+            OleDbCommandBuilder builder = new OleDbCommandBuilder(da);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "testTable");
+            dataGridView1.DataSource = ds.Tables[0];
+            conn.Close();
         }
     }
 }
