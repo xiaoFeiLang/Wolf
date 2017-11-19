c0d7d53cdf634933f9dc0a16405f8cec3ecdd4cc
627265625@qq.com <xiao99L>
2017-11-19 16:14:38 +0800

    new common testConsole testWebform testMVC

---
+++ TestWebForm/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TestWebForm.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ TestWebForm/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWebForm.Startup))]
+namespace TestWebForm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
